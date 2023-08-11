e120aeed4a8b6f53b115fe9cf1cc711828eab791
Madan Sain <madansain@Madans-Mac-mini.local>
2023-08-11 21:32:16 +0530

    add filter chnages

--- components/SearchManufacher.tsx
+++ components/SearchManufacher.tsx
@@ -22,7 +22,7 @@ const SearchManufacher = ({
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={maunfacher} onChange={setmaunfacher}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -61,7 +61,19 @@ const SearchManufacher = ({
                 }
                 value={item}
               >
-                {item}
+              {({ selected, active }) => (
+                      <>
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                          {item}
+                        </span>
+
+                        {/* Show an active blue background color if the option is selected */}
+                        {selected ? (
+                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
               </Combobox.Option>
             ))}
           </Combobox.Options>
